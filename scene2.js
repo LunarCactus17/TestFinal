6c57aad41e935374a91a7a6fd115e6043a387836
Xuhui Zou <150887554+LunarCactus17@users.noreply.github.com>
2025-08-01 22:00:26 -0400

    Add files via upload

--- scene2.js
+++ scene2.js
@@ -3,17 +3,22 @@ class scene2 extends Phaser.Scene {
         super("playGame");
         this.score = 0;
         this.lives = 3;
+        this.bulletDelay = 200;
+        this.lastBulletTime = 0;
     }
 
     create(){
+        // Background
         this.background = this.add.tileSprite(0,0, config.width, config.height, "background");
         this.background.setOrigin(0,0);
 
+        // Player Ship
         this.ship = this.physics.add.sprite(config.width/2, config.height/2 + 200, "ship");
         this.ship.play("ship_animation");
         this.ship.setCollideWorldBounds(true);
         this.ship.body.setAllowGravity(false);
 
+        // Player Input
         this.cursorKeys = this.input.keyboard.createCursorKeys();
         this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         
@@ -22,22 +27,27 @@ class scene2 extends Phaser.Scene {
         this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
+        // Player Bullets
         this.bullets = this.physics.add.group({
-            classType: Bullet,
-            runChildUpdate: true
+            defaultKey: 'beam',
+            maxSize: 10
         });
 
+        // Asteroids and UFOs
         this.asteroids = this.physics.add.group();
         this.ufo = this.physics.add.sprite(200, 0, "ufo").setScale(4);
         this.ufo.play("ufo_animation");
         this.ufo.body.setAllowGravity(false);
 
+        // Health/Lives
         this.heart1 = this.add.image(410, 25, "heart").setScale(.1);
         this.heart2 = this.add.image(440, 25, "heart").setScale(.1);
         this.heart3 = this.add.image(470, 25, "heart").setScale(.1);
         
+        // Score Text
         this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '20px', fill: '#fff' });
 
+        // Timer to spawn new asteroids
         this.asteroidSpawnTimer = this.time.addEvent({
             delay: 1000,
             callback: this.spawnAsteroid,
@@ -45,14 +55,15 @@ class scene2 extends Phaser.Scene {
             loop: true
         });
         
+        // Collision detection
         this.physics.add.collider(this.bullets, this.asteroids, this.hitAsteroid, null, this);
         this.physics.add.collider(this.ship, this.asteroids, this.shipHit, null, this);
         this.physics.add.collider(this.ship, this.ufo, this.shipHit, null, this);
     }
 
-    update(){
+    update(time){
         this.background.tilePositionY -= 0.5;
-        this.moveShipManager();
+        this.moveShipManager(time);
         this.moveUFO(this.ufo, .2);
 
         this.asteroids.children.each(asteroid => {
@@ -60,9 +71,15 @@ class scene2 extends Phaser.Scene {
                 this.resetAsteroidPos(asteroid);
             }
         });
+        
+        this.bullets.children.each(bullet => {
+            if (bullet.y < 0) {
+                bullet.disableBody(true, true);
+            }
+        });
     }
 
-    moveShipManager() {
+    moveShipManager(time) {
         this.ship.body.setVelocityX(0);
         this.ship.body.setVelocityY(0);
 
@@ -78,15 +95,18 @@ class scene2 extends Phaser.Scene {
             this.ship.body.setVelocityY(310);
         }
         
-        if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+        if (this.spacebar.isDown && time > this.lastBulletTime) {
             this.fireBullet();
+            this.lastBulletTime = time + this.bulletDelay;
         }
     }
     
     fireBullet() {
-        let bullet = this.bullets.get(this.ship.x, this.ship.y - 20);
+        let bullet = this.bullets.get();
         if (bullet) {
-            bullet.fire();
+            bullet.enableBody(true, this.ship.x, this.ship.y - 20, true, true);
+            bullet.setVelocityY(-400);
+            bullet.setScale(0.5);
         }
     }
 
@@ -144,32 +164,5 @@ class scene2 extends Phaser.Scene {
         ufo.y = 0;
         var randomX = Phaser.Math.Between(0, config.width);
         ufo.x = randomX;
-    }
-}
-
-class Bullet extends Phaser.GameObjects.Image {
-    constructor(scene) {
-        super(scene, 0, 0, 'beam'); 
-        this.speed = 400;
-        this.setActive(false);
-        this.setVisible(false);
-        this.setScale(0.5);
-        
-        this.body.setAllowGravity(false);
-    }
-
-    fire() {
-        this.body.reset(this.x, this.y);
-        this.setActive(true);
-        this.setVisible(true);
-        this.body.velocity.y = -this.speed;
-    }
-    
-    preUpdate(time, delta) {
-        super.preUpdate(time, delta);
-        if (this.y <= 0) {
-            this.setActive(false);
-            this.setVisible(false);
-        }
     }
 }
